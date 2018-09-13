8b705437ad069eb359e48cfb697096aaf45395fa
unknown <1433452759@qq.com>
2018-09-13 10:35:56 +0800

    init project

---
+++ app/src/main/java/zny/hjs/com/testrxpermissions/MainActivity.java
@@ -0,0 +1,26 @@
+package zny.hjs.com.testrxpermissions;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_request)
+    Button btnRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_request)
+    public void onViewClicked() {
+    }
+}
