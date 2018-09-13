5441ea46e6fa7ff1f4f4a2c0ac5bd42f1fb12173
unknown <1433452759@qq.com>
2018-09-13 10:40:05 +0800

    hjs

--- app/src/main/java/zny/hjs/com/testrxpermissions/MainActivity.java
+++ app/src/main/java/zny/hjs/com/testrxpermissions/MainActivity.java
@@ -1,12 +1,17 @@
 package zny.hjs.com.testrxpermissions;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+
+import com.tbruyelle.rxpermissions.RxPermissions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_request)
     public void onViewClicked() {
+        //这个请求事件我写在点击事件里面，
+        //点击button之后RxPermissions会为我们申请运行时权限
+        RxPermissions.getInstance(MainActivity.this)
+                .request(Manifest.permission.READ_CALENDAR)//这里填写所需要的权限
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean aBoolean) {
+                        if (aBoolean) {//true表示获取权限成功（注意这里在android6.0以下默认为true）
+                            Log.i("permissions", Manifest.permission.READ_CALENDAR + "：" + "获取成功");
+                        } else {
+                            Log.i("permissions", Manifest.permission.READ_CALENDAR + "：" + "获取失败");
+                        }
+                    }
+                });
+
     }
 }
