0471b571495dbe62e988a6ae17a67f72fa3754d6
unknown <1433452759@qq.com>
2018-09-13 10:54:44 +0800

    多个权限

--- app/src/main/java/zny/hjs/com/testrxpermissions/MainActivity.java
+++ app/src/main/java/zny/hjs/com/testrxpermissions/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 
+import com.tbruyelle.rxpermissions.Permission;
 import com.tbruyelle.rxpermissions.RxPermissions;
 
 import butterknife.BindView;
@@ -30,14 +31,24 @@ public class MainActivity extends AppCompatActivity {
         //这个请求事件我写在点击事件里面，
         //点击button之后RxPermissions会为我们申请运行时权限
         RxPermissions.getInstance(MainActivity.this)
-                .request(Manifest.permission.READ_CALENDAR)//这里填写所需要的权限
-                .subscribe(new Action1<Boolean>() {
+                //分别申请多个权限时，使用requestEach
+                .requestEach(Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.CAMERA)
+                .subscribe(new Action1<Permission>() {
                     @Override
-                    public void call(Boolean aBoolean) {
-                        if (aBoolean) {//true表示获取权限成功（注意这里在android6.0以下默认为true）
-                            Log.i("permissions", Manifest.permission.READ_CALENDAR + "：" + "获取成功");
-                        } else {
-                            Log.i("permissions", Manifest.permission.READ_CALENDAR + "：" + "获取失败");
+                    public void call(Permission permission) {
+                        if (permission.name.equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                            //当ACCESS_FINE_LOCATION权限获取成功时，permission.granted=true
+                            Log.i("permissions", Manifest.permission.ACCESS_FINE_LOCATION + "：" + permission.granted);
+                        }
+                        if (permission.name.equals(Manifest.permission.RECORD_AUDIO)) {
+                            //当RECORD_AUDIO 权限获取成功时，permission.granted=true
+                            Log.i("permissions", Manifest.permission.RECORD_AUDIO + "：" + permission.granted);
+                        }
+                        if (permission.name.equals(Manifest.permission.CAMERA)) {
+                            //当CAMERA权限获取成功时，permission.granted=true
+                            Log.i("permissions", Manifest.permission.CAMERA + "：" + permission.granted);
                         }
                     }
                 });
